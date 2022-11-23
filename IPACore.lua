0a12e84b751674759a33a2569313bdfa16b09673
fuba <q3fuba@gmail.com>
2022-11-23 04:41:14 +0100

    Initial commit

---
+++ IPACore.lua
@@ -0,0 +1,190 @@
+local IPAUIDebug=false
+
+function InstancePortalAdvUI_OnLoad(self)
+	LoadAddOn("Blizzard_WorldMap")
+	self:RegisterEvent("ADDON_LOADED")
+
+	IPAUIPrintDebug("InstancePortalAdvUI_OnLoad()")
+	WorldMapFrame:AddDataProvider(CreateFromMixins(IPAInstancePortalMapDataProviderMixin));
+	hooksecurefunc("ToggleDropDownMenu", IPAUIDropDownInit)
+
+end
+
+function InstancePortalAdvUI_OnEvent(event, arg1)
+	if event == "ADDON_LOADED" then
+		if IPAUITrackInstancePortals == nil then
+			IPAUIPrintDebug("IPUISetDefaults()")
+			IPAUITrackInstancePortals = true
+			IPAUITrackInstancePortalsOnContinents = true
+		end
+		
+		IPAUIPrintDebug("ADDON_LOADED()")
+
+		RegisterCVar("IPAUITrackInstancePortals")
+		RegisterCVar("IPAUITrackInstancePortalsOnContinents")
+	end
+end
+
+function IPAUIDropDownInit(_, _, dropDownFrame, _, _, _, _, clickedButton)
+	local trackingOptionsFrame = WorldMapFrame.overlayFrames[2]
+	local trackingOptionsMenu = trackingOptionsFrame.DropDown
+
+	IPAUIPrintDebug("IPAUIDropDownInit")
+
+	local function OnSelection(button)
+		if button.value == "IPAUITrackInstancePortals" then
+			IPAUITrackInstancePortals = button.checked
+		else
+			IPAUITrackInstancePortalsOnContinents = button.checked
+		end
+
+		SetCVar(button.value, button.checked and "1" or "0", "INSTANCE_PORTAL_REFRESH");
+	end
+
+	if dropDownFrame == trackingOptionsMenu then
+
+		local info = UIDropDownMenu_CreateInfo();
+
+		UIDropDownMenu_AddSeparator();
+		info.isTitle = true;
+		info.notCheckable = true;
+		info.text = DUNGEONS.." / "..RAIDS;
+		info.isNotRadio = true;
+		UIDropDownMenu_AddButton(info);
+		
+		info = UIDropDownMenu_CreateInfo();
+		info.isTitle = nil;
+		info.notCheckable = nil;
+		info.text = "Show on Zone Map"; --BATTLEFIELD_MINIMAP
+		info.isNotRadio = true;
+		info.checked = IPAUITrackInstancePortals;
+		info.func = OnSelection;
+		info.keepShownOnClick = true;
+		info.value = "IPAUITrackInstancePortals";
+		--UIDropDownMenu_AddButton(info);
+		
+		info = UIDropDownMenu_CreateInfo();
+		info.isTitle = nil;
+		info.notCheckable = nil;
+		info.text = "Show on Continent Map"; --WORLD_MAP
+		info.isNotRadio = true;
+		info.checked = IPAUITrackInstancePortalsOnContinents;
+		info.func = OnSelection;
+		info.keepShownOnClick = true;
+		info.value = "IPAUITrackInstancePortalsOnContinents";
+		UIDropDownMenu_AddButton(info);
+	end
+end
+
+function IPAUIPrintDebug(t)
+	if (IPAUIDebug) then
+		print(t)
+	end
+end
+
+function IPAUIGetEntranceInfoForMapID(mapID, i)
+
+		instancePortal = IPAUIPinDB[mapID][i]
+		if not (instancePortal) then
+			IPAUIPrintDebug("No instances for map: "..mapID)
+			return nil
+		end
+
+		local x = instancePortal[1]/100
+		local y = instancePortal[2]/100
+		local subInstanceMapIDs = instancePortal[3]
+		local hubName = instancePortal[4]
+		local factionWhitelist = nil
+		local desired_IPAUIInstanceMapDB = IPAUIInstanceMapDB;
+		local playerFaction = UnitFactionGroup("player")
+
+		if hubName == "FactionSpecific" then
+			factionWhitelist = playerFaction;
+			desired_IPAUIInstanceMapDB = IPAUIInstanceFactionSpecificDB[factionWhitelist];
+			hubName = nil
+		elseif hubName == "Alliance" or hubName == "Horde" then
+			factionWhitelist = hubName;
+			desired_IPAUIInstanceMapDB = IPAUIInstanceFactionSpecificDB[factionWhitelist];
+			hubName = nil
+		end
+		
+		if hubName then
+			entranceInfo = {};
+
+			entranceInfo["areaPoiID"] = C_AreaPoiInfo.GetAreaPOIForMap(mapID)[0];
+			entranceInfo["position"] = CreateVector2D(x, y);
+			entranceInfo["name"] = hubName;
+
+			local description = "";
+			local dungeonCount = 0
+			local raidCount = 0
+
+			for m = 1, #subInstanceMapIDs do
+				local instanceID = subInstanceMapIDs[m]
+				local localizedName = EJ_GetInstanceInfo(instanceID);
+				local requiredLevel = desired_IPAUIInstanceMapDB[subInstanceMapIDs[m]][3]
+				local dungonType = desired_IPAUIInstanceMapDB[subInstanceMapIDs[m]][2];
+
+				if dungonType == 1 then
+					dungeonCount=dungeonCount+1
+					description = description..localizedName.." |cFF888888("..LFG_TYPE_DUNGEON..")|r\n"
+				else
+					raidCount=raidCount+1
+					description = description..localizedName.." |cFF888888("..LFG_TYPE_RAID..")|r\n"
+				end
+			end
+
+			entranceInfo["description"] = description;
+
+			if dungeonCount > raidCount then
+				entranceInfo["atlasName"] = "Dungeon";
+			else
+				entranceInfo["atlasName"] = "Raid";
+			end
+
+			entranceInfo["journalInstanceID"] = 0;
+			entranceInfo["hub"] = 1;
+			entranceInfo["factionWhitelist"] = factionWhitelist;
+			
+			IPAUIPrintDebug("Hub: " .. entranceInfo["name"]);
+
+			return entranceInfo
+		end
+
+		local m = 1
+		if desired_IPAUIInstanceMapDB[subInstanceMapIDs[m]] then
+			local name = desired_IPAUIInstanceMapDB[subInstanceMapIDs[m]][1]
+			local type = desired_IPAUIInstanceMapDB[subInstanceMapIDs[m]][2]
+			local requiredLevel = desired_IPAUIInstanceMapDB[subInstanceMapIDs[m]][3]
+			
+			local tier = desired_IPAUIInstanceMapDB[subInstanceMapIDs[m]][4]
+
+			entranceInfo = {};
+
+			entranceInfo["areaPoiID"] = C_AreaPoiInfo.GetAreaPOIForMap(mapID)[0];
+			entranceInfo["position"] = CreateVector2D(x, y);
+			if (type == 1) then
+				entranceInfo["atlasName"] = "Dungeon";
+				entranceInfo["description"] = LFG_TYPE_DUNGEON;
+			else
+				entranceInfo["atlasName"] = "Raid";
+				entranceInfo["description"] = LFG_TYPE_RAID;
+			end
+
+			EJ_SelectTier(tier)
+			local instanceID = subInstanceMapIDs[m]
+
+			local localizedName = EJ_GetInstanceInfo(instanceID);
+
+			entranceInfo["name"] = localizedName.."|r";
+
+			entranceInfo["journalInstanceID"] = instanceID;
+			entranceInfo["tier"] = tier;
+			entranceInfo["hub"] = 0;
+			entranceInfo["factionWhitelist"] = factionWhitelist;
+
+			IPAUIPrintDebug("Instance: " .. entranceInfo["name"].." id:"..instanceID);
+
+			return entranceInfo
+		end
+end
---
+++ IPAInstancePortalPinTemplate.lua
@@ -0,0 +1,179 @@
+IPAInstancePortalMapDataProviderMixin = CreateFromMixins(MapCanvasDataProviderMixin);
+
+function IPAInstancePortalMapDataProviderMixin:RemoveAllData()
+	self:GetMap():RemoveAllPinsByTemplate("IPAInstancePortalPinTemplate");
+end
+
+function IPAInstancePortalMapDataProviderMixin:OnShow()
+	self:RegisterEvent("CVAR_UPDATE");
+end
+
+function IPAInstancePortalMapDataProviderMixin:OnHide()
+	self:UnregisterEvent("CVAR_UPDATE");
+end
+
+function IPAInstancePortalMapDataProviderMixin:OnEvent(event, ...)
+	if event == "CVAR_UPDATE" then
+		local eventName, value = ...;
+		if eventName == "INSTANCE_PORTAL_REFRESH" then
+			self:RefreshAllData();
+		end
+	end
+end
+
+function IPAInstancePortalMapDataProviderMixin:RefreshAllData(fromOnShow)
+	self:RemoveAllData();
+	IPAUIPrintDebug("IPAInstancePortalMapDataProviderMixin:RefreshAllData")
+
+	local trackOnZones = IPAUITrackInstancePortals
+	local trackOnContinents = IPAUITrackInstancePortalsOnContinents
+
+	local mapID = self:GetMap():GetMapID();
+	IPAUIPrintDebug("Map ID = "..mapID)
+
+	local dungeonEntrances = C_EncounterJournal.GetDungeonEntrancesForMap(mapID)
+
+	for i, dungeonEntranceInfo in ipairs(dungeonEntrances) do
+		IPAUIPrintDebug("Atlas = ("..dungeonEntranceInfo["position"]["x"]..","..dungeonEntranceInfo["position"]["y"]..")")
+	end
+
+	if IPAUIPinDB[mapID] then
+		local count = #IPAUIPinDB[mapID]
+		local isContinent = false;
+		for i = 1, #IPAUIContinentMapDB do
+			if IPAUIContinentMapDB[i] == mapID then
+				isContinent = true;
+			end
+		end
+  
+        if not (isContinent) then
+            return
+        end
+		
+		IPAUIPrintDebug("Map is continent = "..(isContinent and 'true' or 'false'))
+		local playerFaction = UnitFactionGroup("player")
+
+		for i = 1, count do
+			local entranceInfo = IPAUIGetEntranceInfoForMapID(mapID, i);
+			
+			if entranceInfo then
+				local factionWhitelist = entranceInfo["factionWhitelist"];
+				
+				local isWhitelisted = true;
+				
+				if factionWhitelist and not (factionWhitelist == playerFaction) then
+					isWhitelisted = false
+				end
+
+				if (isContinent and trackOnContinents) or (not isContinent and trackOnZones) then
+					if (isWhitelisted) then
+						self:GetMap():AcquirePin("IPAInstancePortalPinTemplate", entranceInfo);
+					end
+				end
+			end
+		end
+	end
+
+end
+
+-- local function for "Add Waypoints" with the feature to "force" Built-In Waypoint System, just set "useTomTom" to false or leave it blank
+local function AddWaypoint(mapID, x, y, title, useTomTom)
+  if (useTomTom == true) and TomTom then
+    title = title or "Waypoint"
+    TomTom:AddWaypoint(mapID, x, y, {
+      title = title,
+      persistent = false,
+      minimap = true,
+      world = true,
+      from = "InstancePortals"
+    })
+  else -- if TomTom is not installed or useTomTom is false you the Built-In Waypoint System
+    if C_Map.CanSetUserWaypointOnMap(mapID) then
+      local vector = CreateVector2D(x, y)
+      local mapPoint = UiMapPoint.CreateFromVector2D(mapID, vector)
+      C_Map.SetUserWaypoint(mapPoint)
+      local shouldSuperTrack = not C_SuperTrack.IsSuperTrackingUserWaypoint();
+      C_SuperTrack.SetSuperTrackedUserWaypoint(shouldSuperTrack)
+      if shouldSuperTrack then
+        PlaySound(SOUNDKIT.UI_MAP_WAYPOINT_SUPER_TRACK_ON, nil, SOUNDKIT_ALLOW_DUPLICATES);
+      else
+        PlaySound(SOUNDKIT.UI_MAP_WAYPOINT_SUPER_TRACK_OFF, nil, SOUNDKIT_ALLOW_DUPLICATES);
+      end
+    end
+  end
+end
+
+
+--[[ Pin ]]--
+IPAInstancePortalProviderPinMixin = BaseMapPoiPinMixin:CreateSubPin("PIN_FRAME_LEVEL_DUNGEON_ENTRANCE");
+
+function IPAInstancePortalProviderPinMixin:OnAcquired(dungeonEntranceInfo) -- override
+	BaseMapPoiPinMixin.OnAcquired(self, dungeonEntranceInfo);
+
+	self.hub = dungeonEntranceInfo.hub
+	self.tier = dungeonEntranceInfo.tier;
+	self.journalInstanceID = dungeonEntranceInfo.journalInstanceID;
+end
+
+function IPAInstancePortalProviderPinMixin:OnClick(button)
+	local useWaypoints = true
+  local useTomTom = true
+  local wp_mapid, wp_x, wp_y, wp_name
+  useTomTom = useTomTom and (TomTom ~= nil) or false
+
+	IPAUIPrintDebug("IPAInstancePortalProviderPinMixin:OnClick, button: "..button)
+	IPAUIPrintDebug("IPAInstancePortalProviderPinMixin:OnClick, self.hub: "..self.hub)
+	
+  if self.hub == 0 then
+    if (button == "LeftButton" and IsShiftKeyDown() and useWaypoints == true) then
+			if IsControlKeyDown() then useTomTom = false end
+      local uiMapID = self:GetMap():GetMapID();
+			if Debug and Debug == true then print("uiMapID: "..uiMapID) end
+      if not uiMapID then return end			
+      local mapChildren = C_Map.GetMapChildrenInfo(uiMapID, Enum.UIMapType.Zone) -- get current map children
+      if ( (type(mapChildren) ~= 'table') or (#mapChildren < 1) ) then return end -- mapChildren is not table or empty
+      local journalInstanceID = self.journalInstanceID
+			if Debug and Debug == true then print("journalInstanceID: "..journalInstanceID) end
+      if not journalInstanceID then return end
+
+      for _, childMapInfo in ipairs(mapChildren) do -- enum "current" map for children
+        if childMapInfo and childMapInfo.mapID then
+          local dungeonEntrances = C_EncounterJournal.GetDungeonEntrancesForMap(childMapInfo.mapID); -- get Dungeon Entrances for current Map
+          for _, dungeonEntranceInfo in ipairs(dungeonEntrances) do -- enum "dungeonEntrances"
+            if dungeonEntranceInfo.journalInstanceID == journalInstanceID then -- found Dungeon with matching instanceID
+              wp_mapid = childMapInfo.mapID
+              wp_x = dungeonEntranceInfo.position.x
+              wp_y = dungeonEntranceInfo.position.y
+              wp_name = dungeonEntranceInfo.name or "Waypoint"
+							IPAUIPrintDebug("childMapInfo.mapID: "..childMapInfo.mapID)
+							IPAUIPrintDebug("dungeonEntranceInfo.name: "..dungeonEntranceInfo.name)
+							IPAUIPrintDebug("dungeonEntranceInfo.position.x: "..dungeonEntranceInfo.position.x)
+							IPAUIPrintDebug("dungeonEntranceInfo.position.y: "..dungeonEntranceInfo.position.y)
+						end
+          end
+				end
+      end
+
+      
+      if (not wp_mapid) then -- if no "Dungeon Entrance" is found, try to use Map Pin itself as Source
+        wp_mapid = self:GetMap():GetMapID();
+        wp_x, wp_y = self:GetPosition()
+        wp_name = self.name or "Waypoint"
+      end
+    else -- not "RightButton" or "useWaypoints" is false then just open Encounter Journal
+      EncounterJournal_LoadUI();
+      EncounterJournal_OpenJournal(nil, self.journalInstanceID)
+    end
+  else -- if self.hub ~= 0, try to use Map Pin itself as Source
+    if (button == "LeftButton" and IsShiftKeyDown() and useWaypoints == true) then
+      wp_mapid = self:GetMap():GetMapID();
+      wp_x, wp_y = self:GetPosition()
+      wp_name = self.name or "Waypoint"
+    end
+  end
+
+	-- check for all needed Variables and Add Waypoint if all Variables are present
+  if (button == "LeftButton" and IsShiftKeyDown() and useWaypoints == true) and wp_mapid and wp_x and wp_y and wp_name then
+    AddWaypoint(wp_mapid, wp_x, wp_y, wp_name, useTomTom)
+  end
+end
